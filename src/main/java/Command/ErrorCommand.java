ead1a1f67d4967388f1be45f69a6164f9fc2b636
AaronLuk <chihengluk@gmail.com>
2019-09-30 21:30:08 +0800

    Update UI

--- src/main/java/Command/ErrorCommand.java
+++ src/main/java/Command/ErrorCommand.java
@@ -16,14 +16,14 @@ public class ErrorCommand extends Command{
     public void execute(TaskList tasks, Storage storage, UI ui) {
         taskMessage = ui.line() + "\n";
         taskMessage += "Command does not exist\nThese are the commands:\n";
-        taskMessage += "-list to list tasks\n";
-        taskMessage += "-done <number> to find task\n";
-        taskMessage += "-delete <number> to delete task\n";
-        taskMessage += "-sort <number> to list all tasks with specified priority\n";
-        taskMessage += "-find <word> to find task with specified word\n";
-        taskMessage += "-todo <description> <priority> to add todo task reminder\n";
-        taskMessage += "-deadline <description> <priority> /by <DD-MM-YYYY HH:MM> to add deadline reminder\n";
-        taskMessage += "-event <description> <priority> /at <DD-MM-YYYY HH:MM> to add event reminder\n";
+        taskMessage += "$list to list tasks\n";
+        taskMessage += "$done <number> to find task\n";
+        taskMessage += "$delete <number> to delete task\n";
+        taskMessage += "$sort <number> to list all tasks with specified priority\n";
+        taskMessage += "$find <word> to find task with specified word\n";
+        taskMessage += "$todo <description> <priority> to add todo task reminder\n";
+        taskMessage += "$deadline <description> <priority> /by <DD-MM-YYYY HH:MM> to add deadline reminder\n";
+        taskMessage += "$event <description> <priority> /at <DD-MM-YYYY HH:MM> to add event reminder\n";
         taskMessage += ui.line();
 
     }
